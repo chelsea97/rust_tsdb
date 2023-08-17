d78df0197ab321ad5a0447101127a7bb615ee9ab
chelsea97 <chelsea97@126.com>
2023-08-17 11:38:30 +0800

    update

--- memory_layout/src/main.rs
+++ memory_layout/src/main.rs
@@ -26,6 +26,26 @@ struct SimpleVec_2<T>{
     data:* mut T, //动态指针
 }
 
+struct MiniVec<T>{
+    // len, capacity, T...
+    data: *mut(usize,usize,T)
+}
+
+struct MySmallVec<T,const N:usize>{
+    data:MySmallVecData<T,N>,
+    capacity:usize 
+    //if capacity > N, heap structure
+    //else stack structure
+}
+//save boolean variable
+struct  BitVec{
+    bits:Vec<i64>
+}
+
+struct VecOption<T>{
+
+}
+
 enum Data {
     //tag,8
     I64(i32),
@@ -38,11 +58,19 @@ enum Option {
     None,
 }
 
-type OptData = Option<Data>;
+enum DataWithMiniVec {
+    I32(i32),
+    F64(f64),
+    Bytes(MiniVec<u8>)
+}
+
+//type OptData = Option<Data>;
 fn main() {
     println!("Hello, world!");
     println!("size of i32:{}",std::mem::size_of::<i32>());
     println!("size of i64:{}",std::mem::size_of::<i64>());
     println!("size of [i32:4]:{}",std::mem::size_of::<[i32;4]>());
-
+    //vec::new不会分配内存
+    let vec: Vec<u8> = Vec::new();
+    assert_eq!(vec.capacity(),0); 
 }
